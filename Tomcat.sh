437c8859f5ddbd4d5259c343c610f250dc3cc4b6
SirishaBathina <141664760+SirishaBathina@users.noreply.github.com>
2024-07-17 10:22:04 +0530

    Update Tomcat.sh

--- Tomcat.sh
+++ Tomcat.sh
@@ -1,1 +1,63 @@
+### Install java
+```sh
+sudo yum install java-11-amazon-corretto-devel -y
+```
+```sh
+java --version 
+```
+```sh
+yum update
+```
+##To install tomcat
+  ```sh
+  cd /opt/
+```
+```sh
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.90/bin/apache-tomcat-9.0.90.tar.gz
+```
+```sh
+tar -zvxf apache-tomcat-9.0.90.tar.gz
+```
+```sh
+cd apache-tomcat-9.0.90
+```
+```sh
+cd bin/
+```
+```sh
+chmod +x startup.sh
+```
+```sh
+chmod +x shutdown.sh
+```
 
+# To start Tomcat
+```sh
+./startup.sh
+```
+
+After
+```sh
+cd /path/to/tomcat/webapps/manager/META-INF/context.html
+```
+value tag un comment
+( <!--
+--> )
+
+
+cd conf
+
+vi tomcat-users.xml
+
+#Add below lines between <tomcat-users> tag
+```sh
+
+<role rolename="manager-gui"/>
+<role rolename="manager-script"/>
+<role rolename="manager-jmx"/>
+<role rolename="manager-status"/>   
+<user username="admin" password="admin" roles="manager-gui,manager-script,manager-jmx,manager-status"/>
+<user username="deployer" password="deployer" roles="manager-script"/>
+<user username="tomcat" password="s3cret" roles="manager-gui"/>
+```
+http://server_ip:8080/
